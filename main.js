a251d03ceac78f331254ab258d9d4497c5569eb1
guss-renteria <gusstavo.renteria.g@gmail.com>
2022-05-24 22:36:03 +0000

    se configuro la sección 'Intro'

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 // [*] estilos
 import './styles/normalize.style.css'
 import './styles/index.style.scss'
-// ? header
-import './styles/header.style.scss'
+//
+import './styles/main.styles.scss'
 
 // [*] iconos
 import './icons/icomoon/style.css'
 
 // [+]
 import Header from './scripts/header.script'
+import Intro from './scripts/intro.script'
 
+//
 const __header = document.querySelector('header')
 const header = new Header(__header)
-header.debug()
+
+const __intro = document.querySelector('main > section.intro')
+const intro = new Intro(__intro)
+intro.debug()
+
+// [*] eventos
+// ? onresize
+window.onresize = () => {
+  intro.resize( window.innerWidth )
+}
---
+++ scripts/intro.script.js
@@ -0,0 +1,36 @@
+import '../styles/header.style.scss'
+
+import background_mobile from '../images/mobile/image-header.jpg'
+import background_desktop from '../images/desktop/image-header.jpg'
+
+class Intro {
+  is_mobile
+
+  constructor( context ) {
+    this.intro = context
+
+    this.load()
+    this.resize(window.innerWidth)
+  }
+  // [-]
+  load() {
+    this.background = document.querySelector('main > section.intro > img.background')
+  }
+  // [+]
+  resize(size) {
+    if(size < 768 && (this.is_mobile == undefined || !this.is_mobile)) {
+      this.is_mobile = true
+      this.background.src = background_mobile
+    }else if(size >= 768 && (this.is_mobile == undefined || this.is_mobile)) {
+      this.is_mobile = false
+      this.background.src = background_desktop
+    }
+  }
+
+  debug() {
+    console.log(`[+] intro: ${ this.intro }`)
+    console.log(`[+] background: ${ this.background }`)
+  }
+}
+
+export default Intro
